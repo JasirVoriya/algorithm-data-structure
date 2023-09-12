8007e299736a103e54256ee0d54978795f8a7bfe
JasirVoriya <1838566218@qq.com>
2023-09-12 22:04:18 +0800

    整理了文件夹结构，删除了一些无用文件

--- Python/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-a, b, p = map(int, input().split())
-base = a % p
-ans = 1
-while b != 0:
-    if b & 1 == 1:
-        ans = (ans * base) % p
-    base = (base << 1) % p
-    b = b >> 1
-print(ans)
--- 瞎搞/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-int main()
-{
-    int i = 8;
-    printf("%d %d %d %d %d ", i++, ++i, --i, i--, ++i); // 8 8 7 9 9
-    return 0;
-}
